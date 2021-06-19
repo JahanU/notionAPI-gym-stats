34706f0e72a4ebbc5622a0ccb9b5f21e46c4f70c
Jahan <34219176+JahanU@users.noreply.github.com>
2021-06-19 15:21:21 +0100

    Init

---
+++ index.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors') // Enable Cross-origin resource sharing (Access from any IP)
+
+const app = express();
+// app.use(express.urlencoded({ extended: false })) // parses incoming requests with urlencoded payloads and is based on body-parser.
+app.use(express.json()) // Returns middleware that only parses json. The options are // NEEDED for POST/PUT request
+app.use(express.static('client'));
+app.use(cors());
+
+const { addStatsRow } = require("./notion")
+
+
+app.get('/', (req, res) => {
+  res.send("Hello World, Landing page!");
+});
+
+app.post('/post-gym-stats', async (req, res) => {
+  console.log(req.body);
+
+  const { exercise, weight, tagName } = req.body; // All Strings
+  let tag = [{ name: tagName }]; // Tags need to be an array of objects (in our case, it will always be length = 1)
+
+  try {
+    await addStatsRow({
+      exercise,
+      weight,
+      tags: tag
+    });
+    res.send(req.body);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log('starting server at..', process.env.PORT)
+});
+
---
+++ notion.js
@@ -0,0 +1,63 @@
+// Use import for ES6 modules and require for commonJS.
+const { Client } = require("@notionhq/client");
+
+const notion = new Client({ auth: process.env.NOTION_KEY })
+const DATABASE_ID = process.env.NOTION_DATABASE_ID
+// Ids of columns within the database
+const NOTION_TAGS_ID = 'ihHP'; //  Tags: { id: 'ihHP', type: 'multi_select', multi_select: [Object] },
+const NOTION_DATES_ID = 'sLg@'; // Dates: { id: 'sLg@', type: 'date', date: {} },
+const NOTION_WEIGHT_ID = '|=UC'; //  'Weight (1S * 10R)': { id: '|=UC', type: 'rich_text', rich_text: {} },
+const NOTION_EXERCISE_ID = 'title'; //  Exercises: { id: 'title', type: 'title', title: {} }
+
+// Notion Page = Page or database entry, as that is also a page!
+function addStatsRow({ exercise, weight, tags }) {
+  try {
+    notion.pages.create({
+      parent: { database_id: DATABASE_ID },
+
+      properties: {
+        [NOTION_EXERCISE_ID]: { // In order to set object keys from variables you need to wrap them in brackets.
+          title: [ // Type
+            {
+              "text": {
+                "content": exercise
+              }
+            }
+          ]
+        },
+        [NOTION_WEIGHT_ID]: {
+          rich_text: [
+            {
+              "text": {
+                "content": weight
+              }
+            }
+          ]
+        },
+        [NOTION_DATES_ID]: {
+          date: {
+            "start": new Date().toISOString()
+          }
+        },
+        [NOTION_TAGS_ID]: { // // If we select 1 item, it becomes an object and not array. Must pass in array tho!
+          multi_select: tags.map(tag => {
+            return { name: tag.name }
+          })
+        }
+      },
+    });
+    console.log('Row added!');
+  } catch (error) {
+    console.log('func addStatsRow - Recieved error: ', error.body);
+  }
+}
+
+// let tagEx = {
+//   name: 'Push',
+// };
+// addStatsRow( { exercise: "LEG DAY", weight: "100KG", tags: [tagEx] } )
+
+
+module.exports = { // Export these functions to use in index.js
+  addStatsRow,
+}
