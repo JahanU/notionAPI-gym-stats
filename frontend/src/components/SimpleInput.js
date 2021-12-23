560d6da287948a47403775a52f038ff9a5439e4b
Jahan <34219176+JahanUoL@users.noreply.github.com>
2021-12-23 16:50:07 +0000

    simple way to manage small form fields

--- frontend/src/components/SimpleInput.js
+++ frontend/src/components/SimpleInput.js
@@ -1,9 +1,29 @@
+import { useRef, useState } from 'react';
+
+
 const SimpleInput = (props) => {
+
+  const nameRef = useRef('');
+  const [name, setName] = useState('');
+
+  const nameInputHandler = (e) => {
+    setName(e.target.value);
+  }
+
+  const submitFormHandler = (e) => {
+    e.preventDefault();
+    console.log(name);
+
+    console.log(nameRef.current.value)
+  }
+
   return (
-    <form>
+    <form onSubmit={submitFormHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          ref={nameRef}
+          onChange={nameInputHandler} type='text' id='name' />
       </div>
       <div className="form-actions">
         <button>Submit</button>
